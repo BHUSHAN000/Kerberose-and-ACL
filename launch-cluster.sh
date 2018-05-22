7ebdee1cd6719dc1e47d163ee4532237fc70fe82
BHUSHAN000 <bhushan.hdfs@gmail.com>
2018-05-22 14:56:27 +0530

    Cloudera Director and insecurity

---
+++ launch-cluster.sh
@@ -0,0 +1,4 @@
+#export AWS_ACCESS_KEY_ID=AKIAIOTDNYSIO3S6JVTA
+#export AWS_SECRET_ACCESS_KEY=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
+
+cloudera-director bootstrap-remote ./aws.simple.conf --lp.remote.username=admin --lp.remote.password=admin --lp.remote.hostAndPort=localhost:7189
